e85c66d0185aee51ed2ba07939a2ad3456e20b04
Nyxorion <siiraxmc@gmail.com>
2025-07-07 16:14:01 -0500

    update Webs my first updte

--- app/src/main/java/com/tami/tareas/View/menus/Web.kt
+++ app/src/main/java/com/tami/tareas/View/menus/Web.kt
@@ -3,7 +3,6 @@ package com.tami.tareas.View.menus
 //importaciones para los links
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.net.Uri
 import android.util.Log
 import android.widget.Toast
 import androidx.compose.ui.platform.LocalContext
